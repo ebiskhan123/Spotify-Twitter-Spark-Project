6af4b86ab04b03a24ad4e4799ea614039544287c
tanaysaxena97 <tanaysaxena97@gmail.com>
2022-04-01 15:53:34 -0400

    added twitter API code

--- src/main/java/com/csye7200/application/services/ServiceManager.java
+++ src/main/java/com/csye7200/application/services/ServiceManager.java
@@ -3,6 +3,7 @@ package com.csye7200.application.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -17,6 +18,7 @@ public class ServiceManager {
     @Autowired
     TwitterService twitterService;
 
+    @PostConstruct
     public void scheduleBatchProcessing(){
         ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
         service.scheduleAtFixedRate(() -> spotifyService.getData(),0,10, TimeUnit.MINUTES);
--- src/main/java/com/csye7200/application/services/TwitterService.java
+++ src/main/java/com/csye7200/application/services/TwitterService.java
@@ -1,11 +1,28 @@
 package com.csye7200.application.services;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TwitterService implements ServiceInterface {
     @Override
     public void getData() {
-
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://api.twitter.com/2/tweets/search/recent?query=%23music")
+                .method("GET", null)
+                .addHeader("Authorization", "Bearer ")
+                .addHeader("Cookie", "guest_id=v1%3A164883870855697149; guest_id_ads=v1%3A164883870855697149; guest_id_marketing=v1%3A164883870855697149; personalization_id=\"v1_YCqtednQLZfgvw7isPLFdg==\"")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().string());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
