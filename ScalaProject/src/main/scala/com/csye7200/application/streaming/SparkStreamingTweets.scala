b40075b5c07e02fc5ad3394425fe5d09ee02fe87
vasudhabangard <bangard.v@northeastern.edu>
2022-04-15 17:25:06 -0400

    performed sentiment analysis on each row of lyrics dataframe

--- ScalaProject/src/main/scala/com/csye7200/application/streaming/SparkStreamingTweets.scala
+++ ScalaProject/src/main/scala/com/csye7200/application/streaming/SparkStreamingTweets.scala
@@ -1,7 +1,7 @@
 package com.csye7200.application.streaming
 
 import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
-import org.apache.spark.sql.{Column, SparkSession, functions, types}
+import org.apache.spark.sql.{Column, Dataset, ForeachWriter, Row, SparkSession, functions, types}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.functions.struct
 import org.apache.spark.sql.types.{ArrayType, StringType, StructField, StructType}
@@ -20,10 +20,9 @@ object SparkStreamingTweets {
     val df = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", "127.0.0.1:9092")
-      .option("subscribe", "tweets-topic")
+      .option("subscribe", "songs-topic")
       .option("startingOffsets", "earliest")
       .load()
-
 
     val schema = new StructType()
       .add("songDetails",
@@ -31,13 +30,25 @@ object SparkStreamingTweets {
           .add("trackName",StringType)
           .add("lyrics",StringType)))
       .add("senderId",StringType)
-      .add("tweets",StringType)
+      .add("tweets",
+        ArrayType(new StructType()
+        .add("id", StringType)
+        .add("text", StringType)))
 
-    val songs = df.select(from_json(col("value"),schema))
-    val songexpanded = songs.withColumn("songExpanded",explode_outer(col("songDetails")))
-    val sentiment = udf(twitterAnalysis.intSentiment _)
-    val r = songexpanded.withColumn("songSentiment",sentiment(col("songExpanded.lyrics")))
+    val songs = df.selectExpr("cast (value as string)").select(from_json(col("value"),schema).as("data")).select("data.*")
+    val songexpanded = songs.select(explode(col("songDetails") as "songDetails"))
+    val r = songs.select(explode(col("songDetails.lyrics") ))
 
+    def saveToFile() = (df : Dataset[Row], batchId: Long) => {
+      df.persist()
+      df.foreach((row: Row) => {println(twitterAnalysis.intSentiment(row.toString()))})
+    }
+
+    r.writeStream
+      .outputMode("append")
+      .foreachBatch(saveToFile())
+      .start()
+      .awaitTermination()
 
   }
 }
