58a7d5e0e3408f8603b63cb404064206e1fb6c9b
vasudhabangard <bangard.v@northeastern.edu>
2022-04-26 14:10:55 -0400

    bug fix

--- Dashboard/src/main/java/com/csye7200/application/repository/SongRepository.java
+++ Dashboard/src/main/java/com/csye7200/application/repository/SongRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface SongRepository extends CrudRepository<Song,Long> {
+public interface SongRepository extends CrudRepository<Song,String> {
 
     @Query("update Song set processed = processed + 1 where title = $title ")
     Integer updateCount( String title);
