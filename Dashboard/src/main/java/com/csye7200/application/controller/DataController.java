fc28cba5365fd67fd4e5832e9787256d62efa8b8
vasudhabangard <bangard.v@northeastern.edu>
2022-04-27 00:15:51 -0400

    enabled cors

--- Dashboard/src/main/java/com/csye7200/application/controller/DataController.java
+++ Dashboard/src/main/java/com/csye7200/application/controller/DataController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import java.util.List;
 @Service
 @RestController
 @RequestMapping("/v1")
+@CrossOrigin
 public class DataController {
 
     @Autowired
